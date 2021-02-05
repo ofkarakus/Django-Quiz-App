1f3e4d65c4c51775ea9e41c7f54f33b31616668e
Omer Karakus <omerfarukkarakus01@gmail.com>
2021-02-05 10:33:42 +0000

    add django-cors-headers

--- src/project/settings.py
+++ src/project/settings.py
@@ -47,12 +47,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'nested_admin',
     'rest_framework.authtoken',
-    'dj_rest_auth'
+    'dj_rest_auth',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -143,3 +145,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOW_ALL_ORIGINS = True
